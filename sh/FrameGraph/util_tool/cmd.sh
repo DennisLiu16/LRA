0a14529221ddce57e1c88ed2e1adf2a0fe6b47d3
DennisLiu16 <liusx880630@gmail.com>
2022-06-24 17:55:34 +0800

    Add sh dir for util_tool FrameGraph
    Usage:
    cd /data/FrameGraph/<some_dir>
    ~/LRA/sh/FrameGraph/util_tool/cmd.sh <binary_file_name>
    
    Default:
    1000 Hz
    60s
    svg file name: <binary_file_name>_<start_time>.svg
    remove all process file e.g. data, perf files

---
+++ sh/FrameGraph/util_tool/cmd.sh
@@ -0,0 +1,32 @@
+# $1: program name
+# you should run this sh in data folder
+
+pid=$(pidof $1)
+
+if [ -z "$pid" ]
+then
+    echo "can't find pid of $1"
+    exit
+else
+    echo -e "pid of $1 is: $pid"
+fi
+
+sampling_rate=1000
+echo "sampling rate is: $sampling_rate (Hz)"
+
+monitor_time=60
+echo "monitor time is: $monitor_time (s)"
+
+time=$(date +"%T")
+echo "start at: $time"
+
+sudo perf record -F $sampling_rate -p $pid -g -o in-fb.data -- sleep $monitor_time
+sudo perf script -i in-fb.data > in-fb.perf
+
+/opt/FlameGraph/stackcollapse-perf.pl in-fb.perf > in-fb.folded
+/opt/FlameGraph/flamegraph.pl in-fb.folded > "$1_$time.svg"
+
+# remove
+rm -f in-fb.data
+rm in-fb.perf
+rm in-fb.folded
