834f34c5ffaef98ae0edd4249dabf5aef3f05c05
DennisLiu16 <liusx880630@gmail.com>
2022-07-05 16:52:01 +0800

    LRA log util version 1

--- src/util/log/log.cc
+++ src/util/log/log.cc
@@ -1,1 +1,36 @@
 #include <util/log/log.h>
+
+namespace lra_log_util {
+
+// implement of LogUnit
+// public
+LogUnit::~LogUnit() {}
+
+void LogUnit::AddLogger(std::string logger_name) {  // move copy value to unordered set directly
+  loggers_.insert(std::move(logger_name));
+}
+
+void LogUnit::RemoveLogger(std::string logger_name) {  // move copy value to unordered set directly
+  loggers_.erase(std::move(logger_name));
+}
+
+bool LogUnit::IsRegistered(std::string logunit_name) {
+  return logunits_.count(std::move(logunit_name));
+}
+
+// private
+void LogUnit::Register(std::shared_ptr<LogUnit> ptr) { logunits_[ptr->name_] = ptr; }
+
+// 要搞清楚順序
+// 放在 ~LogUnit 會錯是因為只有當 use_count == 0 也就是物件不存在時才會 call destructor -> invalid
+// operation ? used in class' destruor
+void LogUnit::Drop() {  // should use when shared_ptr.use_count() == 1, so Drop->~LogUnit()
+  std::cout << "left " << logunits_[name_].use_count() << std::endl;
+  std::cout << "remove " + name_ << std::endl;
+  logunits_.erase(name_);
+}
+
+// static variable init
+uint32_t LogUnit::idx_for_next_ = 0;
+
+}  // namespace lra_log_util
--- src/util/log/log.h
+++ src/util/log/log.h
@@ -4,7 +4,8 @@
 // see details at https://hackmd.io/gAsWsT1RSUulPcq4de_HGQ#LRA-log-system
 
 // typeid issue
-// ref: https://stackoverflow.com/questions/281818/unmangling-the-result-of-stdtype-infoname
+// ref:
+// https://stackoverflow.com/questions/281818/unmangling-the-result-of-stdtype-infoname
 
 // test region
 
@@ -12,7 +13,7 @@
 // Macro for spdlog, should placed above include spdlog files
 
 #pragma message("Log system enable: use spdlog")
-// #define LOG_TRACE(,...)
+
 
 #else
 #pragma message("Log system disable")
@@ -21,18 +22,93 @@
 
 #include <spdlog/spdlog.h>
 
+#include <boost/core/demangle.hpp>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+
 namespace lra_log_util {
 
-class LogManager {
+class LogUnit {
  public:
-  LogManager() = default;
-  ~LogManager() = default;
+  inline static std::unordered_map<std::string, std::shared_ptr<LogUnit>> logunits_{};
 
-  // apply log to all loggers - spdlog
-  // log to stdout - stdlog
-  void Log();
+  // LogManager::RegisterLogUnit(getName()); the
+  // name is unique so no need to set string;
+  inline std::string getName() { return name_; }
 
-  void Log2();
+  LogUnit(auto &obj) {
+    // get name
+    std::string class_name;
+    if (!obj) {
+      class_name = "global";
+    } else {
+      class_name = "class_" + boost::core::demangle(typeid(obj).name());
+    }
+    // increment idx_for_next_
+    name_ = class_name.append(1, '_').append(std::to_string(idx_for_next_++));
+    // important: https://zh-blog.logan.tw/2020/03/22/cxx-17-inline-variable/
+  }
+
+  ~LogUnit();
+
+  static std::shared_ptr<LogUnit> CreateLogUnit(auto obj) {
+    auto ptr = std::make_shared<LogUnit>(obj);
+    Register(ptr);
+    return std::move(ptr);
+  }
+
+  static std::shared_ptr<LogUnit> CreateLogUnit() { return CreateLogUnit(nullptr); }
+  static bool IsRegistered(std::string logunit_name);
+
+  void AddLogger(std::string logger_name);
+  void RemoveLogger(std::string logger_name);
+  void Drop();
+
+
+  template <typename... Args>
+  void Log(int level, Args &&...args) {
+
+    // level check
+    if(!(SPDLOG_ACTIVE_LEVEL > level)) {return;}
+
+    for (const auto &logger : loggers_) { // string type
+      if (std::shared_ptr<spdlog::logger> log_ptr = spdlog::get(logger); log_ptr != nullptr) {
+        switch (level) {
+          case spdlog::level::trace:
+            SPDLOG_LOGGER_TRACE(log_ptr, std::forward<Args>(args)...);
+            break;
+          case spdlog::level::debug:
+            SPDLOG_LOGGER_DEBUG(log_ptr, std::forward<Args>(args)...);
+            break;
+          case spdlog::level::info:
+            SPDLOG_LOGGER_INFO(log_ptr, std::forward<Args>(args)...);
+            break;
+          case spdlog::level::warn:
+            SPDLOG_LOGGER_WARN(log_ptr, std::forward<Args>(args)...);
+            break;
+          case spdlog::level::err:
+            SPDLOG_LOGGER_ERROR(log_ptr, std::forward<Args>(args)...);
+            break;
+          case spdlog::level::critical:
+            SPDLOG_LOGGER_CRITICAL(log_ptr, std::forward<Args>(args)...);
+            break;
+        }
+      } else {  // remove logger because it's not valid anymore
+        loggers_.erase(logger);
+      }
+    }
+  }
+
+ private:
+  static uint32_t idx_for_next_; // not static inline
+
+  std::string name_;                         // unique LogUnit name
+  std::unordered_set<std::string> loggers_;  // use string instead of spdlog::logger for
+                                             // shared_ptr drop problem
+
+  static void Register(std::shared_ptr<LogUnit> ptr);
 };
 
 }  // namespace lra_log_util
