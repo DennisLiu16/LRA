0e8c48059d852865890e0b0817c45e499b0b8fc5
DennisLiu16 <liusx880630@gmail.com>
2022-07-07 11:24:04 +0800

    Update log_test code

--- test/log_test/log_test.cc
+++ test/log_test/log_test.cc
@@ -1,17 +1,15 @@
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <util/log/log.h>
 
 #include <iostream>
 
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/sinks/basic_file_sink.h>
-
 std::string ptr2_name;
+std::shared_ptr<lra_log_util::LogUnit> global_ptr;
 
 void func() {
   std::shared_ptr<lra_log_util::LogUnit> ptr2 = lra_log_util::LogUnit::CreateLogUnit();
-  // if a lot of logunit will generate -> remember to use Drop() deregister the logunit
   ptr2_name = ptr2->getName();
-  ptr2->Drop(); // good
 }
 
 int main() {
@@ -19,23 +17,25 @@ int main() {
   func();
 
   // TODO: 把 logunits 放到 private 建立 interface
-  if(auto ptr2_ = lra_log_util::LogUnit::getLogUnitPtr(ptr2_name); ptr2_ != nullptr)
-    spdlog::fmt_lib::print("{}\n", ptr2_.use_count());  
+  if (auto ptr2_ = lra_log_util::LogUnit::getLogUnitPtr(ptr2_name); ptr2_ != nullptr) spdlog::fmt_lib::print("{}\n", ptr2_.use_count());
 
-  auto my_logger = spdlog::basic_logger_mt("console", "/home/ubuntu/LRA/data/log/log_test.log");
-  my_logger->set_level(spdlog::level::err);
-  //spdlog::stdout_color_mt("console");
+  auto my_logger = spdlog::basic_logger_mt("console", "/home/ubuntu/LRA/data/log/log_test.log", true);
+  my_logger->set_level(spdlog::level::trace);
+
+  auto vec = lra_log_util::LogUnit::getAllKeys();
+  for(auto s: vec) {
+    spdlog::fmt_lib::print("{}\n", s);
+  }
 
   ptr->AddLogger("console");
-  int i,j = 0;
-  while(i++ < 4000) {
-    ptr->Log(std::make_tuple("test"), spdlog::level::err);
+  int i = 0, j = 0;
+  while (i++ < 4000) {
+    ptr->Log(std::make_tuple("{}:test", i), spdlog::level::trace);
   }
 
   spdlog::set_default_logger(spdlog::get("console"));
 
-  while(j++ < 4000) {
-    spdlog::error("test");
+  while (j++ < 4000) {
+    spdlog::trace("test");  
   }
-
 }
