cd609418c6d3efda8c4d5b872f7e690dc166d395
DennisLiu16 <liusx880630@gmail.com>
2022-08-06 18:36:21 +0800

    Solve perfect forwarding to template function
    When we use std::forward to class member template function. Keyword
    template is required to explictily declare that it's a member function,
    not a member data.
    
    e.g.
    
      template <auto Method, typename... Args>
      ssize_t Write(Args&&... args) {
        return static_cast<T*>(this)->template WriteImpl<Method>(std::forward<Args>(args)...);
      }
    
      ...
    
      template <I2c::I2cMethod method>
      ssize_t I2c::WriteImpl(const void* data) {
        return WriteMultiImpl<method>(data);
      }
    
    It's will get compiling error if we miss keyword "template" before
    WriteImpl.

--- src/bus/bus.h
+++ src/bus/bus.h
@@ -1,9 +1,11 @@
 #ifndef LRA_BUS_H_
 #define LRA_BUS_H_
 
+#include <fcntl.h>
 #include <memory/registers/registers.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include <util/concepts/concepts.h>
-#include <util/log/log.h>
 #include <util/terminal/terminal.h>
 
 // TODO: include fnctl ...
@@ -17,35 +19,37 @@ using ::lra::memory::registers::is_valid_type_val;
 template <typename T>
 class Bus {
  public:
-  std::shared_ptr<lra::log_util::LogUnit> logunit_ = nullptr;  // try to eliminate this
-
   // CRTP interface
 
   // make Method enum in every bus class, and pass them as first template param
+
 
   template <typename... Args>
   bool Init(Args&&... args) {
     return static_cast<T*>(this)->InitImpl(std::forward<Args>(args)...);  // more general form
   }
 
+  // Important: use template keyword make Method valid
+  // keyword "template" in template function explicitly declare the method is a template class function
+  // Without template, Method is unknown util the template function instantiates, leading compile error
   template <auto Method, typename... Args>
   ssize_t Write(Args&&... args) {
-    return static_cast<T*>(this)->WriteImpl<Method>(std::forward<Args>(args)...);
+    return static_cast<T*>(this)->template WriteImpl<Method>(std::forward<Args>(args)...);
   }
 
   template <auto Method, typename... Args>
   ssize_t WriteMulti(Args&&... args) {
-    return static_cast<T*>(this)->WriteMultiImpl<Method>(std::forward<Args>(args)...);
+    return static_cast<T*>(this)->template WriteMultiImpl<Method>(std::forward<Args>(args)...);
   }
 
   template <auto Method, typename... Args>
   ssize_t Read(Args&&... args) {
-    return static_cast<T*>(this)->ReadImpl<Method>(std::forward<Args>(args)...);
+    return static_cast<T*>(this)->template ReadImpl<Method>(std::forward<Args>(args)...);
   }
 
   template <auto Method, typename... Args>
   ssize_t ReadMulti(Args&&... args) {
-    return static_cast<T*>(this)->ReadMultiImpl<Method>(std::forward<Args>(args)...);
+    return static_cast<T*>(this)->template ReadMultiImpl<Method>(std::forward<Args>(args)...);
   }
 
  protected:
