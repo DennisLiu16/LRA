47d852a1e46d7ba33bf77f07bbed22e916abf0ea
Dennis Liu <liusx880630@gmail.com>
2023-08-21 12:15:52 +0800

    Add updateUSBip shell

---
+++ test/usb_test/script/updateUSBip.sh
@@ -0,0 +1,5 @@
+sudo apt install linux-tools-virtual hwdata
+sudo update-alternatives --install /usr/local/bin/usbip usbip `ls /usr/lib/linux-tools/*/usbip | tail -n1` 22
+
+# reboot cmd
+cd /mnt/c/ && cmd.exe /c start "rebooting WSL" cmd /c "timeout 5 && wsl -d $WSL_DISTRO_NAME" && wsl.exe --terminate $WSL_DISTRO_NAME
